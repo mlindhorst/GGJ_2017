3e01c7db8c98d189308e36928f79e644074ff674
mlindhorst <mlindhorst.24587@gmail.com>
2017-01-20 22:25:47 -0700

    dun dun dadaddun dun dadadun dun dadadaduuuuun UDN

---
+++ KillerWave/Assets/UserControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour {
+
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow) && transform.position.y < 30)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + movementSpeed, transform.position.z);
+        }
+	}
+}
